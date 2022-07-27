bef0ca6888c619cbbe9ac0b53e406b631ae46c1a
LesVu <johndie005@gmail.com>
2022-07-27 15:35:00 +0800

    First

---
+++ src/layouts/Default.vue
@@ -0,0 +1,19 @@
+<template>
+<v-container>
+      <v-app-bar color="blue" dense elevation="1" rounded>
+        <g-link class="nav__link" to="/">Home</g-link>
+        <g-link class="nav__link" to="/about/">About</g-link>
+      </v-app-bar>
+    <slot/>
+  </v-container>
+</template>
+
+<static-query>
+query {
+  metadata {
+    siteName
+  }
+}
+</static-query>
+
+
